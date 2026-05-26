a9ddd68f693dea14638c43360abb3ab748fb9c9e
bhargavachandrainampudi-cell <bhargavachandra.inampudi@gmail.com>
2026-05-26 13:29:49 -0400

    Refactor setup and loop functions in main.ino
    Removed doubleNumber function and related logic.

--- main.ino
+++ main.ino
@@ -1,19 +1,7 @@
-int doubleNumber(int number)
-{
-int result = number * 2;
-return result; 
-}
-int a=4;
-int real
-void setup() {
-  real = doubleNumber(a);
-  pinMode(real, OUTPUT);
+void setup (){
+
 }
 
-void loop() {
-  digitalWrite(real, HIGH);
-  delay(1000);
-  digitalWrite(real, LOW);
-  delay(1000);
-}
+void loop (){
 
+}
